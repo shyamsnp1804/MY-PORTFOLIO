2e31e1e25f2779fab5f071908e4579b104135c05
shyamsnp1804 <snp18032004@gmail.com>
2026-01-23 15:21:04 +0530

    added some projects to project section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,19 +12,19 @@ const Projects = () => {
 
   const projects = [
     {
-      title: "Portfolio Website",
-      desc: "Personal portfolio built with React and Tailwind CSS, featuring dark mode and responsive design.",
-      link: "#",
+      title: "CalSci | Open Source Contribution (Deployed Product)",
+      desc: "Contributing to an actively deployed programmable scientific calculator platform for engineers and researchers. Implemented frontend enhancements for CalSci.io, improving UI responsiveness and usability. Collaborated via GitHub with fork-based workflows and merged pull requests.",
+      link: "https://github.com/I-am-nagesh/CalSci-website",
     },
     {
       title: "Spotify Clone",
-      desc: "A full-stack MERN application.",
+      desc: "Built a Spotify-inspired music streaming UI using React.js and component-driven architecture. Implemented Redux for centralized state management and optimized rendering using hooks. Used useState, useEffect, and useCallback to manage state, side effects, and performance.",
       link: "https://github.com/shyamsnp1804/spotify-clone",
     },
     {
-      title: "Recipe Finder",
-      desc: "Search recipes using external API with React, context, and search filtering.",
-      link: "https://github.com/shyamsnp1804/Recipe-App-Veggie-Bite",
+      title: "SIH Alumni Hub | Smart India Hackathon (SIH25017)",
+      desc: "Designed a centralized alumni engagement platform addressing fragmented alumni data. Implemented signup/signin using browser LocalStorage",
+      link: "https://alumnihub071.netlify.app/",
     },
   ];
 
@@ -50,7 +50,8 @@ const Projects = () => {
           <h2 className="text-4xl font-bold uppercase">Projects</h2>
           <div className="w-24 h-1 bg-purple-500 mx-auto my-4 rounded-full"></div>
           <p className="text-gray-600 dark:text-gray-300 text-lg font-medium max-w-2xl mx-auto">
-            A showcase of the projects I have worked on, highlighting my skills and experience in various technologies
+            A showcase of the projects I have worked on, highlighting my skills
+            and experience in various technologies
           </p>
         </motion.div>
 
@@ -68,7 +69,9 @@ const Projects = () => {
               <h3 className="text-2xl font-semibold text-pink-600 dark:text-pink-400 mb-2">
                 {project.title}
               </h3>
-              <p className="text-gray-700 dark:text-gray-300 mb-4">{project.desc}</p>
+              <p className="text-gray-700 dark:text-gray-300 mb-4">
+                {project.desc}
+              </p>
               <a
                 href={project.link}
                 target="_blank"
