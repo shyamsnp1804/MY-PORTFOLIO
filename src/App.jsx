048c5a2f39c4bdf230e06cd02a14e3b4ff01b934
shyamsnp1804 <snp18032004@gmail.com>
2026-05-09 23:37:41 +0530

    added cursor effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import Navbar from "./components/Navbar";
 import AppRoutes from "./routes/AppRoutes";
+import SplashCursor from "./components/SplashCursor";
 
 function App() {
   return (
-    <div className="min-h-screen bg-white dark:bg-black text-gray-900 dark:text-white transition-colors duration-300">
+    <div className="relative min-h-screen overflow-hidden bg-white dark:bg-black text-gray-900 dark:text-white transition-colors duration-300">
+      {/* Fluid Cursor Effect */}
+      <SplashCursor
+        DENSITY_DISSIPATION={3.5}
+        VELOCITY_DISSIPATION={2}
+        PRESSURE={0.1}
+        CURL={3}
+        SPLAT_RADIUS={0.2}
+        SPLAT_FORCE={6000}
+        COLOR_UPDATE_SPEED={10}
+        SHADING
+        RAINBOW_MODE={false}
+        COLOR="#A855F7"
+      />
+
       <Navbar />
       <AppRoutes />
     </div>
